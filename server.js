a8dd6f6ac1b9c36ae1472fd4f43ffda3fd56966a
Miguel <miguel@nathandigital.com>
2023-11-09 14:13:46 +0800

    Updated code

--- server.js
+++ server.js
@@ -1,43 +1,58 @@
+const https = require('https')
 const http = require('http')
 
-const server = http.createServer((req, res) => {
-    if (req.url == '/getTimeStories' && req.method == 'GET') {
-        const stories = [
-            {
-                "title": "Amy Schneider’s Jeopardy! Streak Ends at 40 Consecutive Wins and $1.4 Million",
-                "link": "https://time.com/6142934/amy-schneider-jeopardy-streak-ends/"
-            },
-            {
-                "title": "'Writing With Fire' Shines a Light on All-Women News Outlet",
-                "link": "https://time.com/6142628/writing-with-fire-india-documentary/"
-            },
-            {
-                "title": "Moderna Booster May Wane After 6 Months",
-                "link": "https://time.com/6142852/moderna-booster-wanes-omicron/"
-            },
-            {
-                "title": "Pressure Mounts for Biden to Nominate a Black Woman to the Supreme",
-                "link": "https://time.com/6142743/joe-biden-supreme-court-nominee-black-woman-campaign-promise/"
-            },
-            {
-                "title": "The James Webb Space Telescope Is in Position—And Now We Wait",
-                "link": "https://time.com/6142769/james-webb-space-telescope-reaches-l2/"
-            },
-            {
-                "title": "We Urgently Need a New National COVID-19 Response Plan",
-                "link": "https://time.com/6142718/we-need-new-national-covid-19-response-plan/"
-            }
-        ]
-        res.setHeader('Content-Type', 'application/json')
-        res.setHeader('Access-Control-Allow-Origin', '*')
-        res.end(JSON.stringify(stories))
-    } else {
-        res.writeHead(404, { 'Content-Type': 'text/plain' })
-        res.end('Not Found')
+const getLatestStories = (callback) => {
+  const url = 'https://time.com/'
+
+  https.get(url, (response) => {
+    let data = ''
+
+    response.on('data', (chunk) => {
+      data += chunk
+    })
+
+    response.on('end', () => {
+      const stories = parseLatestStories(data)
+      callback(stories)
+    })
+  }).on('error', (error) => {
+    console.error(`Error fetching data: ${error}`)
+  })
+}
+const parseLatestStories = (htmlData) => {
+  const stories = []
+  const itemRegex = /<li class="latest-stories__item">(.*?)<\/li>/gs
+  const titleRegex = /<h3 class="latest-stories__item-headline">(.*?)<\/h3>/
+  const linkRegex = /<a\s+(?:[^>]*?\s+)?href=(["'])(.*?)\1/
+
+  let match
+  while ((match = itemRegex.exec(htmlData)) !== null) {
+    const item = match[1]
+    const titleMatch = item.match(titleRegex)
+    const linkMatch = item.match(linkRegex)
+
+    if (titleMatch && linkMatch) {
+      const title = titleMatch[1]
+      const link = 'https://time.com' + linkMatch[2]
+      stories.push({ title, link })
     }
-});
+  }
+
+  return stories
+}
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/getTimeStories') {
+    getLatestStories((data) => {
+      res.setHeader('Content-Type', 'application/json')
+      res.setHeader('Access-Control-Allow-Origin', '*')
+      res.writeHead(200)
+      res.end(JSON.stringify(data))
+    })
+  }
+})
 
 const PORT = 3001
 server.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`)
-});
+  console.log(`Server is running on port ${PORT}`)
+})
